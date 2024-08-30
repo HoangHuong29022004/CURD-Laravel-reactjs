99c9e42423707d82b585c5c297d074395d20f621
HoangHuong29022004 <phamhoanghuong04@gmail.com>
2024-08-30 16:12:23 +0700

    save

--- resources/js/components/Navigation.jsx
+++ resources/js/components/Navigation.jsx
@@ -4,7 +4,7 @@ export default function Navigation() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <NavLink className="navbar-brand" to="/">Laravel React CRUD</NavLink>
+                <NavLink className="navbar-brand" to="/">Thêm sửa xóa laravel react</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
